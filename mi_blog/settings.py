1fa7c01385a3df93f017466751308efc9e997b7d
deyby01 <camachodeybygerman@gmail.com>
2025-07-01 11:54:39 -0400

    feat: Se usa SQLite para pruebas

--- mi_blog/settings.py
+++ mi_blog/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
